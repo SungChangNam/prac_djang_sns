7c4cfdedf8a03168bebd8721a57c2ad8b55290ba
Sung_cn_2 <112399821+SungChangNam@users.noreply.github.com>
2023-01-11 22:12:08 +0900

    [add]articles_CRUD

--- sns/urls.py
+++ sns/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', views.base_response,name='first_test'),
     path('first/', views.first_view,name='first_view'),
-    path('', include('user.urls'))
+    path('', include('user.urls')),
+    path('', include('tweet.urls')),
 ]
--- user/views.py
+++ user/views.py
@@ -1,31 +1,34 @@
 from django.shortcuts import render,redirect
 from .models import UserModel
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model #사용자 데이터베이스 안에 있는지 확인
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def sign_up_view(request):
     if request.method == 'GET':
-        return render(request, 'user/signup.html')
+        user = request.user.is_authenticated
+        if user:
+            return redirect('/')
+        else:
+            return render(request, 'user/signup.html')
     elif request.method =='POST':
         username = request.POST.get('username',None)
-        pasword = request.POST.get('password',None)
-        pasword2 = request.POST.get('password2',None)
+        password = request.POST.get('password',None)
+        password2 = request.POST.get('password2',None)
         bio = request.POST.get('bio',None)
         
         
-        if pasword != pasword2:
+        if password != password2:
             return render(request,'user/signup.html')
         else:
-            exist_user = UserModel.objects.filter(username=username)
+            exist_user = get_user_model().objects.filter(username=username)
             if exist_user:
                 return render(request, 'user/signup.html') #사용자 존재 있으면 나타나는 페이지 
             else:
-                new_user = UserModel()
-                new_user.username =username
-                new_user.password =pasword
-                new_user.password =pasword2
-                new_user.bio =bio
-                new_user.save()
+                UserModel.objects.create_user(username=username,password=password,bio=bio)
+
                 return redirect('/sign-in/')
 
 
@@ -34,12 +37,19 @@ def sign_in_view(request):
         username = request.POST.get('username',None)
         password  = request.POST.get('password',None)
         
-        
-        me = UserModel.objects.get(username=username)
-        if me.password == password:
-            request.session['user'] = me.username
-            return HttpResponse(me.username)
+        me =auth.authenticate(request, username=username, password=password)
+        if me is not None:
+            auth.login(request,me)
+            return redirect('/')
         else:
             return redirect('/sign-in/')
     elif request.method == 'GET':
-        return render(request, 'user/signin.html')
+        user = request.user.is_authenticated
+        if user:
+            return redirect('/')
+        else:
+            return render(request, 'user/signin.html')
+@login_required
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
